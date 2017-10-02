8a3059aa92c9d01c7d17edeba1b2800c0e2e0da2
danymochtar <32459488+danymochtar@users.noreply.github.com>
2017-10-02 22:03:39 +0700

    Initial commit

---
+++ js/main2.js
@@ -0,0 +1,50 @@
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+(function() {
+
+	var bodyEl = document.body,
+		content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function inits() {
+		initsEvents();
+	}
+
+	function initsEvents() {
+		openbtn.addEventListener( 'click', toggleMenus );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenus );
+		}
+
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenus();
+			}
+		} );
+	}
+
+	function toggleMenus() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	inits();
+
+})();
